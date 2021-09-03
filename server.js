ddd93233033f90070390312e343a6c407d3cc97c
Edro <ahmededrees30@gmail.com>
2021-09-03 17:37:48 +0200

    add home route

--- server.js
+++ server.js
@@ -60,6 +60,10 @@ app.post("/send-message", (req, res) => {
   res.send("Message sent");
 });
 
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
 app.listen(3000, () => {
   console.log("Server is up on 3000");
 });
